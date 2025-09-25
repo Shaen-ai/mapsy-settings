92d05fa9367b8ba5a05f4c9f75932bc964845cee
Shahen <shahen1@Shahens-MacBook-Pro.local>
2025-09-26 00:33:04 +0400

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -11,8 +10,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        compact: resolve(__dirname, 'compact.html'),
+        main: './index.html',
+        compact: './compact.html',
       },
       output: {
         entryFileNames: 'assets/[name].[hash].js',
